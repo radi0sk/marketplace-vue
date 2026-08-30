3a5a77d0df404231b5ed0cbe4088acc5078175b8
radi0sk <pedroixbalan@hotmail.es>
2026-08-30 12:11:10 -0600

    fix: update CSP directives for TikTok and Pixabay, add missing composite indexes for orders/questions, and set COOP header

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -36,7 +36,7 @@ const userData = ref<any>({
   }
 });
 
-const defaultAvatar = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+const defaultAvatar = "https://ui-avatars.com/api/?name=Usuario&background=0D8ABC&color=fff";
 const avatarFile = ref<File | null>(null);
 const avatarPreview = ref<string | null>(null);
 
